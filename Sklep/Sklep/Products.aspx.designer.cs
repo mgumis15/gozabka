bee4ea35a1b73c497d0c3e1855a0c161d7fe3440
mgumis15 <49537639+mgumis15@users.noreply.github.com>
2019-11-20 21:02:38 +0100

    new database

---
+++ Sklep/Sklep/Products.aspx.designer.cs
@@ -0,0 +1,35 @@
+﻿//------------------------------------------------------------------------------
+// <generowany automatycznie>
+//     Ten kod został wygenerowany przez narzędzie.
+//
+//     Modyfikacje tego pliku mogą spowodować niewłaściwe zachowanie i zostaną utracone
+//     w przypadku ponownego wygenerowania kodu. 
+// </generowany automatycznie>
+//------------------------------------------------------------------------------
+
+namespace Sklep
+{
+
+
+    public partial class Products
+    {
+
+        /// <summary>
+        /// Kontrolka form1.
+        /// </summary>
+        /// <remarks>
+        /// Pole generowane automatycznie.
+        /// By zmodyfikować deklaracje pola przenieś ją z pliku projektanta do pliku codebehind.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlForm form1;
+
+        /// <summary>
+        /// Kontrolka dMain.
+        /// </summary>
+        /// <remarks>
+        /// Pole generowane automatycznie.
+        /// By zmodyfikować deklaracje pola przenieś ją z pliku projektanta do pliku codebehind.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlGenericControl dMain;
+    }
+}
